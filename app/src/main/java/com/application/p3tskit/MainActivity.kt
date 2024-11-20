e2d98950b32df67d5b0e621e9ea0edc967afce04
Syifa Maulida <isitsma11@students.unnes.ac.id>
2024-11-21 00:24:38 +0700

    Added Fragments

--- app/src/main/java/com/application/p3tskit/MainActivity.kt
+++ app/src/main/java/com/application/p3tskit/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.application.p3tskit
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
