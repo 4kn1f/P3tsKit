c83ee869cffa8a5c64ff4feca3fb371a41c16e33
coro-kunnnnnn <finkazhr@gmail.com>
2024-11-25 09:40:15 +0700

    Edit homepage

---
+++ app/src/main/java/com/application/p3tskit/ui/history/HistoryViewModel.kt
@@ -0,0 +1,6 @@
+package com.application.p3tskit.ui.history
+
+import androidx.lifecycle.ViewModel
+
+class HistoryViewModel: ViewModel() {
+}
---
+++ app/src/main/java/com/application/p3tskit/ui/scan/DetailScanViewModel.kt
@@ -0,0 +1,6 @@
+package com.application.p3tskit.ui.scan
+
+import androidx.lifecycle.ViewModel
+
+class DetailScanViewModel: ViewModel() {
+}
