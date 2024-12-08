937dcd5b9a38142253129827a3115cdf26e99512
coro-kunnnnnn <finkazhr@gmail.com>
2024-12-08 17:02:22 +0700

    Set profile

--- app/src/main/java/com/application/p3tskit/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/application/p3tskit/ui/profile/ProfileFragment.kt
@@ -1,28 +1,64 @@
 package com.application.p3tskit.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.application.p3tskit.R
+import com.application.p3tskit.data.pref.AuthPreferences
+import com.application.p3tskit.data.pref.dataStore
+import com.application.p3tskit.databinding.FragmentProfileBinding
+import com.application.p3tskit.ui.ViewModelFactory
+import com.application.p3tskit.ui.register.RegisterActivity
+import kotlinx.coroutines.launch
 
 class ProfileFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    private lateinit var binding: FragmentProfileBinding
+    private val profileViewModel by viewModels<ProfileViewModel> {
+        ViewModelFactory.getInstance(requireContext())
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        observeViewModel()
+        setupLogoutButton()
+
+        return binding.root
     }
 
-    companion object {
+    private fun setupLogoutButton() {
+        binding.llLogout.setOnClickListener {
+            lifecycleScope.launch {
+                profileViewModel.logout()
+                AuthPreferences.getInstance(requireContext()).logout()
+            }
+        }
+    }
 
+    private fun observeViewModel() {
+        profileViewModel.getSession().observe(viewLifecycleOwner) { user ->
+            user?.let {
+                binding.tvUserName.text = it.email
+            }
+        }
+
+        profileViewModel.logout.observe(viewLifecycleOwner) { isLoggedOut ->
+            if (isLoggedOut) {
+                Toast.makeText(requireContext(), "Logged out successfully", Toast.LENGTH_SHORT).show()
+                val intent = Intent(requireContext(), RegisterActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                requireActivity().finish()
+            }
+        }
     }
 }
